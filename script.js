43bc157b2e234941711ada7b530a3a1ed55ece42
Sam Singer <samuel.singer1@gmail.com>
2017-07-12 16:56:07 -0600

    dan tried to help and failed

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+var websiteTitle = document.getElementById("website-title");
+var websiteURL = document.querySelector("#website-url");
+
 function addCards() {
-  var websiteTitle = document.getElementById("website-title").value;
-  var websiteURL = document.querySelector("#website-url").value;
+  var websiteTitleValue = websiteTitle.value;
+  var websiteURLValue = websiteURL.value;
+
   $(".all-boxes").prepend(`<article class='single-box'>
-     <h2>${websiteTitle}</h2>
+     <h2>${websiteTitleValue}</h2>
      <hr>
-     <a href='http://${websiteURL}' class='site-urls' target='_blank'>${websiteURL}</a>
+     <a href='http://${websiteURLValue}' class='site-urls' target='_blank'>${websiteURLValue}</a>
      <hr>
      <button type='button' name='read' class='read-button'>Read</button>
      <button type='button' name='delete' class='delete-button'>Delete</button>
@@ -12,27 +16,32 @@ function addCards() {
 };
 
 function evalInputs() {
-  var websiteTitle = document.getElementById("website-title").value;
-  var websiteURL = document.querySelector("#website-url").value;
-
   if (websiteTitle === "") {
     alert("Please Enter A Website Title");
-    if (_stop) return;
+    // if (_stop) return;
   } else if (websiteURL === ""){
     alert("Please Enter A Website URL");
-    if (_stop) return;
+    // if (_stop) return;
   };
 };
 
+  $('header').on('keyup', '.website-title, .website-url', function() {
+    var websiteTitleValue = websiteTitle.value;
+    var websiteURLValue = websiteURL.value;
+    if( websiteURLValue === "" || websiteTitleValue === ""){
+      $('.enter-button').attr('disabled', true).css("background-color", "#DDD");
+    } else {
+      $('.enter-button').p('disabled', false).css("background-color", "#FFC107");
+    }
+ });
+
 document.querySelector(".enter-button").addEventListener('click', function(event) {
   event.preventDefault()
-
   evalInputs()
   addCards()
 });
 
 $('.all-boxes').on('click', '.read-button', function() {
-  // console.log('clicked');
   $(this).parents('.single-box').toggleClass('read');
 });
 
