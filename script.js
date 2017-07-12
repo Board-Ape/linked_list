b831b07ab0dd28c1b687271d5dbf46c0b5ff3505
Sam Singer <samuel.singer1@gmail.com>
2017-07-11 18:18:15 -0600

    Alert is up and working but throwing code

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 // var $readButton = $('.read-button');
 // var $boxContainer = $('.all-boxes');
+function addCards() {
+  var websiteTitle = document.getElementById("website-title").value;
+  var websiteURL = document.querySelector("#website-url").value;
+  $(".all-boxes").prepend(`<article class='single-box'>
+     <h2>${websiteTitle}</h2>
+     <hr>
+     <a href='http://${websiteURL}' class='site-urls' target='_blank'>${websiteURL}</a>
+     <hr>
+     <button type='button' name='read' class='read-button'>Read</button>
+     <button type='button' name='delete' class='delete-button'>Delete</button>
+     </article>`);
+};
+
+function evalInputs() {
+  var websiteTitle = document.getElementById("website-title").value;
+  var websiteURL = document.querySelector("#website-url").value;
+  var 
+
+  if (websiteTitle === "") {
+    alert("Please Enter A Website Title");
+    if (_stop) return;
+  } else if (websiteURL === ""){
+    alert("Please Enter A Website URL");
+    if (_stop) return;
+  };
+};
 
 document.querySelector(".enter-button").addEventListener('click', function(event) {
   event.preventDefault()
-    var websiteTitle = document.getElementById("website-title").value;
-    var websiteURL = document.querySelector("#website-url").value;
-    $(".all-boxes").prepend(`<article class='single-box'>
-       <h2>${websiteTitle}</h2>
-       <hr>
-       <a href='${websiteURL}' class='site-urls'>${websiteURL}</a>
-       <hr>
-       <button type='button' name='read' class='read-button'>Read</button>
-       <button type='button' name='delete' class='delete-button'>Delete</button>
-       </article>`);
+
+  evalInputs()
+  addCards()
 });
 
   $('.all-boxes').on('click', '.read-button', function() {
