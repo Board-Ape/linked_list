9ede4f0f5c1e7a7c74e790ba9f2c21b15b21ad64
Sam Singer <samuel.singer1@gmail.com>
2017-07-13 11:31:32 -0600

    end eval

--- script.js
+++ script.js
@@ -29,16 +29,22 @@ function totalUnreads() {
 }
 
 function evalInputs() {
-      var websiteTitle = document.querySelector("#website-title").value;
-      var websiteURL = document.querySelector("#website-url").value;
-      if (websiteTitle === "") {
-        return alert("Please enter a website title.");
-      } else if (websiteURL === "") {
-        return alert ("Pease enter a website URL.");
-      } else {
-        addCards();
-      }
-    };
+  var websiteTitle = document.querySelector("#website-title").value;
+  var websiteURL = document.querySelector("#website-url").value;
+  if (!websiteTitle) {
+    return alert("Please enter a website title.");
+  } else if (!websiteURL) {
+    return alert ("Pease enter a website URL.");
+  } else {
+    addCards();
+  }
+};
+
+$('.enter-button').prop('disabled', true).css("background-color", "#ddd");
+
+$('.left-side').on('keyup', '#website-title, #website-url', function(){
+  $('.enter-button').prop('disabled', false).css("background-color", "#FFC107");
+})
 
 document.querySelector(".enter-button").addEventListener('click', function(event) {
   event.preventDefault()
@@ -47,18 +53,22 @@ document.querySelector(".enter-button").addEventListener('click', function(event
   totalUnreads()
 });
 
-$('.all-boxes').on('click', '.read-button', function() {
+$('.all-boxes').on('click', '.read-button', toggleReadCard)
+
+function toggleReadCard() {
   $(this).parents('.single-box').toggleClass('read');
   totalReads()
   totalUnreads()
-});
+};
 
-$('.all-boxes').on('click', '.delete-button', function() {
+$('.all-boxes').on('click', '.delete-button', toggleDeleteCard)
+
+function toggleDeleteCard() {
   $(this).parents('.single-box').remove()
   totalCards()
   totalReads()
   totalUnreads()
-});
+};
 
 $('.clear-all-reads').on('click', function(event) {
   event.preventDefault()
